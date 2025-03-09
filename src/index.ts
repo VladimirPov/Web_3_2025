117fc42f83a447411976004fcc909f1f9a48f5e9
Liho <159879612+VladimirPov@users.noreply.github.com>
2025-03-09 19:26:35 +0300

    Added ejs

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,21 @@ const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+app.set("view engine", "ejs")
+
+app.get('/ejs-pages', (req: Request, res: Response) => {
+  res.render('layout', {title: "ejs-pages", message: "Hi, user!", content: "index", content2: "about"})
+})
+
+app.get('/about', (req: Request, res: Response) => {
+  res.render('layout', {title: "about", content: "about"})
+})
+
+app.get('/', (req: Request, res: Response) => {
+  res.render('layout', {title: "Main", content: "main"})
+})
 
 app.post('/', (req: Request, res: Response) => {
   console.log(req.body)
