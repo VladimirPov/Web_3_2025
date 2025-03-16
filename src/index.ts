9d507097b165d7188571081c2737bb6f3305022b
Liho <159879612+VladimirPov@users.noreply.github.com>
2025-03-16 17:48:52 +0300

    Lab 4

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express , {Request, Response} from "express"
+import mongoose from "mongoose"
+import User from "./models/user"
 const app = express()
 const port = 3000
 
@@ -6,8 +8,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 
-app.set("view engine", "ejs")
+mongoose.connect("mongodb://admin:1234@localhost:27017/ivtb-22?authSource=admin")
+.then(()=>console.log("DB connected!"))
+.catch((err)=>console.error(err))
 
+
+app.set("view engine", "ejs")
 app.get('/ejs-pages', (req: Request, res: Response) => {
   res.render('layout', {title: "ejs-pages", message: "Hi, user!", content: "index", content2: "about"})
 })
@@ -16,7 +22,7 @@ app.get('/about', (req: Request, res: Response) => {
   res.render('layout', {title: "about", content: "about"})
 })
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {y
   res.render('layout', {title: "Main", content: "main"})
 })
 
@@ -24,6 +30,39 @@ app.post('/', (req: Request, res: Response) => {
   console.log(req.body)
   console.log(req.headers)
   res.send('Hello World!')
+})
+
+// Получить всех пользователей
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.status(201).json(users);
+  } catch (e) {
+    res.status(400).json({message: (e as Error).message})
+  }
+})
+
+// Удалить пользователя 
+app.delete("/users", async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    const result = await User.deleteOne(req.body);
+    res.status(201).json(result);
+  } catch (e) {
+    res.status(400).json({message: (e as Error).message})
+  }
+})
+
+// Добавить пользователя
+app.post('/users', async (req: Request, res: Response)=> {
+  try{
+    console.log(req.body)
+    const user = new User(req.body)
+    await user.save()
+    res.status(201).json(user)
+  }catch (e){
+    res.status(400).json({message: (e as Error).message})
+  }
 })
 
 app.listen(port, () => {
