9405298aa96d26c011eef4e6d664ca0c5dd4d87b
Liho <159879612+VladimirPov@users.noreply.github.com>
2025-04-21 16:01:16 +0300

    Added authentication

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express , {Request, Response} from "express"
 import usersRouter from "./routes/users.routes"
+import authRouter from "./routes/auth.routes"
 import { connectDB } from "./config/ConnectDB"
 import swaggerUI from "swagger-ui-express";
 import swaggerSpec from "./config/openAPI";
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 app.use("/users", usersRouter)
+app.use("/auth", authRouter)
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 connectDB()
 
